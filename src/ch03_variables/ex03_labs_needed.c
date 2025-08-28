6377840aa13ec0e39a43f1fb8bb01d70ba84ddeb
minseo0614 <125117397+minseo0614@users.noreply.github.com>
2025-08-28 15:04:13 +0900

    Update ex03_labs_needed.c

--- src/ch03_variables/ex03_labs_needed.c
+++ src/ch03_variables/ex03_labs_needed.c
@@ -4,7 +4,7 @@
 int main(void){
     int n;
     if (scanf("%d", &n) != 1 || n < 0) return 1;
-    int rooms = (n + SIZE - 1) / SIZE; // 올림 나눗셈
+    int rooms = (n + SIZE - 1) / SIZE;
     if (rooms == 0) rooms = 1;
     printf("%d\n", rooms);
     return 0;
