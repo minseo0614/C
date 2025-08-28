c91c00a950c58043b487ce133b51b71d3a57d9ab
minseo0614 <125117397+minseo0614@users.noreply.github.com>
2025-08-28 21:57:35 +0900

    Create ex10_diamond.c

---
+++ ch05_control/ex10_diamond.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void){
+    int h;
+    if (scanf("%d",&h)!=1 || h<1) return 1;
+    for (int i=1;i<=h;i++){
+        for (int sp=0; sp<h-i; sp++) putchar(' ');
+        for (int k=0;k<2*i-1;k++) putchar('*');
+        putchar('\n');
+    }
+    for (int i=h-1;i>=1;i--){
+        for (int sp=0; sp<h-i; sp++) putchar(' ');
+        for (int k=0;k<2*i-1;k++) putchar('*');
+        putchar('\n');
+    }
+    return 0;
+}
